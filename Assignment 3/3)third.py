fe9a3f55072f230cf2379892e3f931e65a303370
khursh1d <106245025+khursh1d@users.noreply.github.com>
2023-10-23 18:01:28 +0500

    3)third.py

--- Assignment 3/3)third.py
+++ Assignment 3/3)third.py
@@ -1,18 +1,27 @@
-sum_of_numbers = 0
+ssum = 0
 count = 0
 
 while True:
-    number = input("Enter a number (or 'done' to exit): ")
-
-    if number == "done":
-        break
-
     try:
-        number = int(number)
-        sum_of_numbers += number
+#Getting the input from the user
+        num = input("Enter a number (or 'done' to exit): ")
+        if num.lower() == "done":
+            break
+        num = float(num)
+        sum += num
         count += 1
+#Printing the error message
     except ValueError:
-        print("Invalid input. Please enter a valid number.")
+        print("Invalid input. Please enter a number or 'done'.")
+#Functions Summ, count and Average number
+if count > 0:
+    average = sum / count
+    print("Sum of input numbers :", sum)
+    print("Number of input :", count)
+    print("Average of input numbers:", average)
+#Error message
+else:
+    print("invalid input. enter a number.")
 
 #Name: Bakhtiyorov Khurshidbek Khamdam Ugli
 #Student ID:202312403
